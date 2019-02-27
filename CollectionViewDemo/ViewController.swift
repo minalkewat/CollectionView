01e8f01a33ad984a4209e677ae6e34d4c7c25fe2
meenal kewat <happykraftsinnovations@gmail.com>
2019-02-27 14:17:42 +0530

    Initial Commit

---
+++ CollectionViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewDemo
+//
+//  Created by Meenal Kewat on 2/27/19.
+//  Copyright © 2019 Happykrafts Innovations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
